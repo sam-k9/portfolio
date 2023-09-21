0da651a6d016eeab30fcb487d1f3d6e03b59e719
Samir <125750491+sam-k9@users.noreply.github.com>
2023-09-21 12:58:45 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,8 +41,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled Front-end Web Developer with expertise in frameworks like
-        React, Node.js, Next.js and Three.js. I'm a quick learner and
+        I'm a skilled Front-end Web Developer with expertise in JavaScript and frameworks like
+        React js, Next.js. I'm a quick learner and
         collaborate closely with clients to create efficient, scalable, and
         user-friendly solutions that solve real-world problems. Let's work
         together to bring your ideas to life!
